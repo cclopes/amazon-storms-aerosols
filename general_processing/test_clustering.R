f9c7ba1a3d2c8e34ce7807b970d221c3e21a28b4
cclopes <camila.lopes@iag.usp.br>
2024-07-19 15:12:26 +0000

    Adding aerosol, clustering processing

---
+++ general_processing/test_clustering.R
@@ -0,0 +1,60 @@
+library(readr)
+library(dplyr)
+library(FactoMineR)
+library(Factoshiny)
+
+# Data with categorical vars
+systems_25km <- read_csv("/home/camilacl/git/amazon-storms-aerosols/data/general/systems_filtered_25km.csv",
+                         col_types = cols(date_init = col_datetime(format = "%Y-%m-%d %H:%M:%S")),
+                         )
+systems_10km <- read_csv("/home/camilacl/git/amazon-storms-aerosols/data/general/systems_filtered_10km.csv", 
+                                  col_types = cols(date_init = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
+
+# Analyzing systems_25km
+systems_25km$area <- as.factor(systems_25km$area)
+systems_25km$reflectivity <- as.factor(systems_25km$reflectivity)
+systems_25km$lifespan <- as.factor(systems_25km$lifespan)
+systems_25km$`sys duration` <- as.factor(systems_25km$`sys duration`)
+systems_25km$season <- as.factor(systems_25km$season)
+systems_25km$`time of day` <- as.factor(systems_25km$`time of day`)
+systems_25km$`electrical activity` <- as.factor(systems_25km$`electrical activity`)
+
+Factoshiny::MFAshiny(systems_25km)
+# Results for above line
+newDF <- systems_25km[,c("sys duration","time of day","season","area","lifespan","max reflectivity","max echotop 0 dBZ","max echotop 20 dBZ","max echotop 40 dBZ","max VIL","max VII","max VIWL","GLD strokes","CAPE","CIN","bl relative humidity","v-wind shear","warm cloud depth","total aerosols","ultrafine aerosols","total CCNs","reflectivity","electrical activity")]
+res.MFA<-MFA(newDF,group=c(3,2,8,5,3,2), type=c("n","n","s","s","s","n"),ncp=3,name.group=c("convection_time","convection_geom","convection_int","initiation_thermo","initiation_aero","supplement_conv"),num.group.sup=c(6),graph=FALSE)
+plot.MFA(res.MFA, choix="ind",lab.par=FALSE,title="Individual factor map")
+plot.MFA(res.MFA, choix="var",habillage='group',title="Correlation circle")
+plot.MFA(res.MFA, choix="group")
+plot.MFA(res.MFA, choix="axes",habillage='group')
+res.HCPC<-HCPC(res.MFA,nb.clust=10,consol=TRUE,graph=FALSE,description=TRUE,order=FALSE)
+plot.HCPC(res.HCPC,choice='tree',title='Hierarchical tree')
+plot.HCPC(res.HCPC,choice='map',draw.tree=FALSE,title='Factor map')
+plot.HCPC(res.HCPC,choice='3D.map',ind.names=FALSE,centers.plot=FALSE,angle=60,title='Hierarchical tree on the factor map')
+
+# Reproducing apps
+Factoshiny::MFAshiny(res.MFA)
+Factoshiny::HCPCshiny(res.MFA)
+
+# Extracting clusters and saving to file
+clusters_systems_25km <- res.HCPC$data.clust
+clusters_systems_25km$name <- systems_25km$name
+clusters_systems_25km %>% 
+  filter(clust %in% c(1,6,9)) %>% 
+  relocate(clust) %>% 
+  relocate(name) %>% 
+  write.csv(
+    "/home/camilacl/git/amazon-storms-aerosols/general_processing/clusters_aero_systems_25km.csv", 
+    row.names = FALSE)
+
+# Analyzing file
+clusters_aero_systems_25km$clust <- as.factor(clusters_aero_systems_25km$clust)
+clusters_aero_systems_25km$area <- as.factor(clusters_aero_systems_25km$area)
+clusters_aero_systems_25km$reflectivity <- as.factor(clusters_aero_systems_25km$reflectivity)
+clusters_aero_systems_25km$lifespan <- as.factor(clusters_aero_systems_25km$lifespan)
+clusters_aero_systems_25km$`sys duration` <- as.factor(clusters_aero_systems_25km$`sys duration`)
+clusters_aero_systems_25km$season <- as.factor(clusters_aero_systems_25km$season)
+clusters_aero_systems_25km$`time of day` <- as.factor(clusters_aero_systems_25km$`time of day`)
+clusters_aero_systems_25km$`electrical activity` <- as.factor(clusters_aero_systems_25km$`electrical activity`)
+tapply(clusters_aero_systems_25km %>% select(!clust), clusters_aero_systems_25km$clust, summary)
+
--- met_aerosol_processing/get_sys_hsp_trajectories.py
+++ met_aerosol_processing/get_sys_hsp_trajectories.py
@@ -7,7 +7,8 @@
 
 
 import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 import pandas as pd
 import geopandas as gpd
@@ -37,52 +38,100 @@ def join_hst_sys(hst, sysfilt, maxdistance):
     """
 
     # Changing CRS to pseudo-Mercator for calculations
-    geo_sysfilt = gpd.GeoDataFrame(sysfilt.to_frame().T, geometry=sysfilt.to_frame().T.geom, crs="EPSG:4326").to_crs(crs=3857)
+    geo_sysfilt = gpd.GeoDataFrame(
+        sysfilt.to_frame().T,
+        geometry=sysfilt.to_frame().T.geom,
+        crs="EPSG:4326",
+    ).to_crs(crs=3857)
     hst.to_crs(crs=3857, inplace=True)
     # Interval between CS timestamp and trajectories points
     inter = abs(geo_sysfilt.iloc[0].timestamp - hst.date)
     # Extract data within intervals <= 6h
     hst = hst.loc[inter[inter <= pd.Timedelta(6, "H")].index]
     # Match in space, return to geographical coordinates
-    pts_within = hst.sjoin_nearest(geo_sysfilt, max_distance=maxdistance, distance_col="distance").reset_index().to_crs(crs="EPSG:4326")
+    pts_within = (
+        hst.sjoin_nearest(
+            geo_sysfilt, max_distance=maxdistance, distance_col="distance"
+        )
+        .reset_index()
+        .to_crs(crs="EPSG:4326")
+    )
     # Interval between CS timestamp and trajectories points
     pts_within["inter"] = abs(pts_within.timestamp - pts_within.date)
     # Match in time, get minimum interval
-    pts_within2 = pts_within.loc[pts_within.groupby("date_hours")["inter"].idxmin()].loc[pts_within.inter < pd.Timedelta(6, "H")]
+    pts_within2 = pts_within.loc[
+        pts_within.groupby("date_hours")["inter"].idxmin()
+    ].loc[pts_within.inter < pd.Timedelta(6, "H")]
     # print(pts_within2)
     # Return row with data or with zeros
     if not pts_within2.empty:
         result = pts_within2.loc[pts_within2["date_hours"].idxmax()]
     else:
-        result = pts_within2.append(pd.Series(0, index=pts_within2.columns), ignore_index=True).iloc[0]
+        result = pts_within2.append(
+            pd.Series(0, index=pts_within2.columns), ignore_index=True
+        ).iloc[0]
     print("done - system", sysfilt.geom_name)
     return result
 
 
 # Reading HYSPLIT trajectories
-df1 = pd.read_csv("/home/camilacl/git/amazon-storms-aerosols/data/hysplit_2014_backward_paths.csv", index_col=[-1])
-df2 = pd.read_csv("/home/camilacl/git/amazon-storms-aerosols/data/hysplit_2015_backward_paths.csv", index_col=[-1])
+df1 = pd.read_csv(
+    "/home/camilacl/git/amazon-storms-aerosols/data/general/hysplit_2014_backward_paths.csv",
+    index_col=[-1],
+)
+df2 = pd.read_csv(
+    "/home/camilacl/git/amazon-storms-aerosols/data/general/hysplit_2015_backward_paths.csv",
+    index_col=[-1],
+)
 # Joining 2014 and 2015 data, remove trajectories longer than 6h
 hst_bwd = pd.concat([df1, df2])
 hst_bwd = hst_bwd.loc[hst_bwd.date_hours >= -6]
 # Converting to geodataframe
-geo_hst_bwd = gpd.GeoDataFrame(hst_bwd, geometry=gpd.points_from_xy(hst_bwd.lon, hst_bwd.lat), crs="EPSG:4326")
+geo_hst_bwd = gpd.GeoDataFrame(
+    hst_bwd,
+    geometry=gpd.points_from_xy(hst_bwd.lon, hst_bwd.lat),
+    crs="EPSG:4326",
+)
 # Converting dates to local time
 geo_hst_bwd.date = pd.to_datetime(geo_hst_bwd.date)
-geo_hst_bwd.date = geo_hst_bwd.date.dt.tz_convert('America/Manaus')
+geo_hst_bwd.date = geo_hst_bwd.date.dt.tz_convert("America/Manaus")
 # Renaming col
 geo_hst_bwd.index.names = ["name_file"]
 print("HYSPLIT trajectories read!")
 
+# Removing trajectories within Manaus plume (30-km radius around INPA)
+# - INPA coordinates
+coords = pd.DataFrame({"lon": [-59.9867], "lat": [-3.0972]})
+geo_coords = gpd.GeoDataFrame(
+    coords, geometry=gpd.points_from_xy(coords.lon, coords.lat), crs="EPSG:4326"
+)
+# - Match with trajectories
+trajs_within_manaus = (
+    geo_hst_bwd.to_crs(crs=3857)
+    .sjoin_nearest(
+        geo_coords.to_crs(crs=3857), max_distance=30000, distance_col="distance"
+    )
+    .index.unique()
+)
+# - Removing trajectories
+geo_hst_bwd = geo_hst_bwd.loc[~geo_hst_bwd.index.isin(trajs_within_manaus)]
+print("Trajectories within Manaus plume removed!")
+
 # Reading systems data
-df = pd.read_pickle("/home/camilacl/git/amazon-storms-aerosols/data/systems_filtered_sg_v1.pickle")
+df = pd.read_pickle(
+    "/home/camilacl/git/amazon-storms-aerosols/data/general/systems_filtered_sg_v1.pickle"
+)
 # Converting to geodataframe
 df.geom = [wkb.loads(bytes(g.ExportToWkb())) for g in df.geom]
 sys_filt = gpd.GeoDataFrame(df, geometry="geom", crs="EPSG:4326")
 print("Systems read!")
 
 # Applying matching function
-sys_hsp = sys_filt.apply(lambda row: join_hst_sys(geo_hst_bwd, row, 25000), axis=1)
+sys_hsp = sys_filt.apply(
+    lambda row: join_hst_sys(geo_hst_bwd, row, 10000), axis=1
+)
 
 # Saving to pickle file
-sys_hsp.loc[sys_hsp.name_file!=0].to_pickle("/home/camilacl/git/amazon-storms-aerosols/data/general/sysfilt_hstbwd_nearest_6h_25km.pickle")
+sys_hsp.loc[sys_hsp.name_file != 0].to_pickle(
+    "/home/camilacl/git/amazon-storms-aerosols/data/general/sysfilt_hstbwd_nearest_6h_10km.pickle"
+)
